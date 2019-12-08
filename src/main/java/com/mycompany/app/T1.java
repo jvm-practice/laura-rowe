a926ee3219a4e8749c06296526ccc92371f1a591
Laura Rowe <55072370+laura-rowe@users.noreply.github.com>
2019-12-08 21:52:43 +0800

    JUnit Demo (#2)
    Resolve #1
    
    Signed-off-by: laura-rowe <laura.luoo@foxmail.com>

---
+++ src/main/java/com/mycompany/app/T1.java
@@ -0,0 +1,9 @@
+package com.mycompany.app;
+
+
+public class T1
+{
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/com/mycompany/app/T1Test.java
@@ -0,0 +1,20 @@
+package com.mycompany.app;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class T1Test
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertEquals(4, T1.add(2, 2));
+    }
+}
